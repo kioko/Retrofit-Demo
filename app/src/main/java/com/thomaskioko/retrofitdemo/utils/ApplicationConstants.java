6fbb73ec0a123d14403c663e677ce445d9ca81a2
kioko <kiokotomas@gmail.com>
2015-11-10 15:37:51 +0300

    Created application constants class

---
+++ app/src/main/java/com/thomaskioko/retrofitdemo/utils/ApplicationConstants.java
@@ -0,0 +1,28 @@
+package com.thomaskioko.retrofitdemo.utils;
+
+/**
+ * @author Kioko
+ * @version Version
+ */
+
+
+public class ApplicationConstants {
+
+    /**
+     * API Endpoint
+     */
+    public static final String END_POINT = "http://api.androidhive.info/json";
+    /**
+     * Connection timeout duration
+     */
+    public static final int CONNECT_TIMEOUT = 60 * 1000;
+    /**
+     * Connection Read timeout duration
+     */
+    public static final int READ_TIMEOUT = 60 * 1000;
+    /**
+     * Connection write timeout duration
+     */
+    public static final int WRITE_TIMEOUT = 60 * 1000;
+
+}
