203d472a16f6cd3bb66a575fd288f043699911e6
kioko <kiokotomas@gmail.com>
2015-11-10 15:42:06 +0300

    Created Movie service class

---
+++ app/src/main/java/com/thomaskioko/retrofitdemo/services/MovieServices.java
@@ -0,0 +1,29 @@
+package com.thomaskioko.retrofitdemo.services;
+
+
+import com.thomaskioko.retrofitdemo.data.Movie;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+
+/**
+ * This class contains API functions
+ *
+ * @author Kioko
+ * @version Version
+ */
+
+
+public interface MovieServices {
+
+    /**
+     * Get movies
+     *
+     * @param movieDayScheduleCallback MovieDaySchedule object
+     */
+    @GET("/movies.json")
+    void getMovies(Callback<List<Movie>> movieDayScheduleCallback);
+
+}
